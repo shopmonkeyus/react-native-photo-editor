623df04ee10783566c1fbcc5cd8eb48994d2e29f
Cesar Garcia <cesar.garcia@shockoe.com>
2023-05-19 10:51:21 -0500

    little code refactor

--- android/src/main/java/com/reactnativephotoeditor/activity/PhotoEditorActivity.kt
+++ android/src/main/java/com/reactnativephotoeditor/activity/PhotoEditorActivity.kt
@@ -63,8 +63,8 @@ import java.io.File
 
 
 open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, View.OnClickListener,
-  PropertiesBSFragment.Properties, ShapeBSFragment.Properties, StickerListener,
-  OnItemSelected, FilterListener {
+  PropertiesBSFragment.Properties, ShapeBSFragment.Properties, StickerListener, OnItemSelected,
+  FilterListener {
   private var mPhotoEditor: PhotoEditor? = null
   private var mProgressDialog: ProgressDialog? = null
   private var mPhotoEditorView: PhotoEditorView? = null
@@ -92,10 +92,9 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
     //intern
     val value = intent.extras
     val path = value?.getString("path")
-    val stickers =
-      value?.getStringArrayList("stickers")?.plus(
-        assets.list("Stickers")!!
-          .map { item -> "/android_asset/Stickers/$item" }) as ArrayList<String>
+    val stickers = value?.getStringArrayList("stickers")?.plus(
+      assets.list("Stickers")!!
+        .map { item -> "/android_asset/Stickers/$item" }) as ArrayList<String>
 //    println("stickers: $stickers ${stickers.size}")
 //    for (stick in stickers) {
 //      print("stick: $stickers")
@@ -129,15 +128,9 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
     mPhotoEditor?.setOnPhotoEditorListener(this)
 //    val drawable = Drawable.cre
 
-    Glide
-      .with(this)
-      .load(path)
-      .listener(object : RequestListener<Drawable> {
+    Glide.with(this).load(path).listener(object : RequestListener<Drawable> {
         override fun onLoadFailed(
-          e: GlideException?,
-          model: Any?,
-          target: Target<Drawable>?,
-          isFirstResource: Boolean
+          e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean
         ): Boolean {
           val intent = Intent()
           intent.putExtra("path", path)
@@ -161,12 +154,14 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
   }
 
   private fun sendEventToRN(name: String) {
-    val reactInstanceManager: ReactInstanceManager = (applicationContext as ReactApplication).reactNativeHost.reactInstanceManager
-    val reactContext: ReactContext? = reactInstanceManager.currentReactContext
-    val params = Arguments.createMap()
-    params.putString("data", name)
+    val reactInstanceManager: ReactInstanceManager =
+      (applicationContext as ReactApplication).reactNativeHost.reactInstanceManager
 
-    reactContext?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)?.emit("EVENT_BARONA", params)
+    val params = Arguments.createMap()
+    params.putString(EVENT_EMITTER_DATA, name)
+
+    reactInstanceManager.currentReactContext?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+      ?.emit(EVENT_EMITTER_EVENT, params)
   }
 
   private fun showLoading(message: String) {
@@ -188,8 +183,7 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
       ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
     if (!isGranted) {
       ActivityCompat.requestPermissions(
-        this, arrayOf(permission),
-        READ_WRITE_STORAGE
+        this, arrayOf(permission), READ_WRITE_STORAGE
       )
     }
   }
@@ -197,8 +191,7 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
   private fun makeFullScreen() {
     requestWindowFeature(Window.FEATURE_NO_TITLE)
     window.setFlags(
-      WindowManager.LayoutParams.FLAG_FULLSCREEN,
-      WindowManager.LayoutParams.FLAG_FULLSCREEN
+      WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN
     )
   }
 
@@ -262,12 +255,15 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
       R.id.imgUndo -> {
         mPhotoEditor!!.undo()
       }
+
       R.id.imgRedo -> {
         mPhotoEditor!!.redo()
       }
+
       R.id.btnSave -> {
         saveImage()
       }
+
       R.id.imgClose -> {
         onBackPressed()
       }
@@ -281,8 +277,7 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
   private fun saveImage() {
     val fileName = System.currentTimeMillis().toString() + ".png"
     val hasStoragePermission = ContextCompat.checkSelfPermission(
-      this,
-      Manifest.permission.WRITE_EXTERNAL_STORAGE
+      this, Manifest.permission.WRITE_EXTERNAL_STORAGE
     ) == PackageManager.PERMISSION_GRANTED
     if (hasStoragePermission || isSdkHigherThan28()) {
       showLoading("Saving...")
@@ -308,8 +303,8 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
           } else {
             mPhotoEditorView?.let {
               val snackBar = Snackbar.make(
-                it, R.string.save_error,
-                Snackbar.LENGTH_SHORT)
+                it, R.string.save_error, Snackbar.LENGTH_SHORT
+              )
               snackBar.setBackgroundTint(Color.WHITE)
               snackBar.setActionTextColor(Color.BLACK)
               snackBar.setAction("Ok", null).show()
@@ -381,6 +376,7 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
         mTxtCurrentTool!!.setText(R.string.label_shape)
         showBottomSheetDialogFragment(mShapeBSFragment)
       }
+
       ToolType.TEXT -> {
         sendEventToRN("on text")
         val textEditorDialogFragment = TextEditorDialogFragment.show(this)
@@ -391,16 +387,19 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
           mTxtCurrentTool!!.setText(R.string.label_text)
         }
       }
+
       ToolType.ERASER -> {
         sendEventToRN("on erase")
         mPhotoEditor!!.brushEraser()
         mTxtCurrentTool!!.setText(R.string.label_eraser_mode)
       }
+
       ToolType.FILTER -> {
         sendEventToRN("on filter")
         mTxtCurrentTool!!.setText(R.string.label_filter)
         showFilter(true)
       }
+
       ToolType.STICKER -> showBottomSheetDialogFragment(mStickerFragment)
     }
   }
@@ -418,17 +417,14 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
     if (isVisible) {
       mConstraintSet.clear(mRvFilters!!.id, ConstraintSet.START)
       mConstraintSet.connect(
-        mRvFilters!!.id, ConstraintSet.START,
-        ConstraintSet.PARENT_ID, ConstraintSet.START
+        mRvFilters!!.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START
       )
       mConstraintSet.connect(
-        mRvFilters!!.id, ConstraintSet.END,
-        ConstraintSet.PARENT_ID, ConstraintSet.END
+        mRvFilters!!.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END
       )
     } else {
       mConstraintSet.connect(
-        mRvFilters!!.id, ConstraintSet.START,
-        ConstraintSet.PARENT_ID, ConstraintSet.END
+        mRvFilters!!.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.END
       )
       mConstraintSet.clear(mRvFilters!!.id, ConstraintSet.END)
     }
@@ -455,5 +451,7 @@ open class PhotoEditorActivity : AppCompatActivity(), OnPhotoEditorListener, Vie
     private val TAG = PhotoEditorActivity::class.java.simpleName
     const val PINCH_TEXT_SCALABLE_INTENT_KEY = "PINCH_TEXT_SCALABLE"
     const val READ_WRITE_STORAGE = 52
+    const val EVENT_EMITTER_DATA = "data"
+    const val EVENT_EMITTER_EVENT = "EVENT_BARONA"
   }
 }
